1227a428a6d1ebcada1205a0a1e2196d11db07f3
Jaden Jo <jaden7856@naver.com>
2021-12-14 10:20:22 +0900

    Update Vagrantfile

--- bash_ssh_conf_4_CentOS.sh
+++ bash_ssh_conf_4_CentOS.sh
@@ -4,5 +4,3 @@ now=$(date +"%m_%d_%Y")
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config_$now.backup
 sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
-apt-get update
-apt-get upgrade -y
